4aa8dc8c9ceb83c53d50763ff0165481785fdf7d
HAH <HAH@HAH>
2017-04-22 00:00:23 +0800

    update

--- src/main/java/com/dodo/multithreading/App.java
+++ src/main/java/com/dodo/multithreading/App.java
@@ -3,8 +3,11 @@ package com.dodo.multithreading;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		for (int i = 1; i <= 10; i++) {
+			Calculator calculator = new Calculator(i);
+			Thread thread = new Thread(calculator);
+			thread.start();
+		}
 	}
 
 }
